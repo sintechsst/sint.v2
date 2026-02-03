95b0f59bc2917917f64589ff156ad3a4f0ded2de
sintechsst <sintech.sst@outlook.com>
2026-02-02 21:51:26 -0300

    Update index.ts

--- src/components/institucional/index.ts
+++ src/components/institucional/index.ts
@@ -1,5 +1,6 @@
-export { default as InstitutionalContainer } from "./institutional-container.tsx"
-export { default as TopbarInstitutional } from "./topbar-institutional.tsx"
-export { default as HeroInstitutional } from "./hero-institutional.tsx"
-export { default as SectionInstitutional } from "./section-institutional.tsx"
-export { default as RodapeInstitutional } from "./rodape-institutional.tsx"
+export { default as InstitutionalContainer } from "./institutional-container"
+export { default as TopbarInstitutional } from "./topbar-institutional"
+export { default as HeroInstitutional } from "./hero-institutional"
+export { default as SectionInstitutional } from "./section-institutional"
+export { default as RodapeInstitutional } from "./rodape-institutional"
+
