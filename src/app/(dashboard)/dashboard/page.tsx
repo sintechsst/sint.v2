2193ada5481dac80a9a58142b8b78a87203d222e
sintechsst <sintech.sst@outlook.com>
2026-02-02 21:46:05 -0300

    Update page.tsx

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import { supabase } from '@/lib/supabase.ts';
+import { supabase } from '@/lib/supabase';
 import { toast } from 'sonner';
 import {
   Users,
