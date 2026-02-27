fbcd3f679a39993ef24000eb06fb196868112805
sintechsst <sintech.sst@outlook.com>
2026-02-27 00:00:27 -0300

    Create layout.tsx

---
+++ src/app/(cliente)/layout.tsx
@@ -0,0 +1,6 @@
+const menuItems = [
+  { label: "Painel", href: "/cliente/dashboard" },
+  { label: "Agendamentos", href: "/cliente/agendamentos" },
+  { label: "Laudos", href: "/cliente/laudos" },
+  { label: "Configurações", href: "/cliente/settings" }
+]
